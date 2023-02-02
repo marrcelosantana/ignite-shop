fcc73893964cfecb9523419e03449035b32a31f5
marcelosantana <marcelosantana@alu.ufc.br>
2023-02-02 18:06:19 -0300

    [Ignite-Shop] - First Configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$green300",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+  cursor: "pointer",
+  fontFamily: "Roboto",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <Button>
+      <span>CLick me</span>
+    </Button>
+  );
 }
